1b542cc6c000d9ad4cfd72f7a13965ce5e59a164
Josh Bailey <josh@vandervecken.com>
2023-10-26 07:54:21 +0000

    no 3.8, yes 3.11

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3,):
     print(
         """You are trying to install chewie on python {py}
 
-chewie is not compatible with python 2, please upgrade to python 3.8 or newer.""".format(
+chewie is not compatible with python 2, please upgrade to python 3.9 or newer.""".format(
             py=".".join([str(v) for v in sys.version_info[:3]])
         ),
         file=sys.stderr,
@@ -20,6 +20,6 @@ chewie is not compatible with python 2, please upgrade to python 3.8 or newer.""
 setup(
     name="chewie",
     setup_requires=["pbr>=1.9", "setuptools>=17.1"],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     pbr=True,
 )
